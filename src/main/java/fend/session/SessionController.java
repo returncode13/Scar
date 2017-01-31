9e80a647ac443da3406790c8aae80d7d0b5d1a70
naila0152 Polarcus <naila0152@naila0152.(none)>
2017-01-31 01:51:17 +0000

    a  bug in P-Q alert fixed. Allows for PQ flags to be unchecked when previously set.

--- src/main/java/fend/session/SessionController.java
+++ src/main/java/fend/session/SessionController.java
@@ -809,6 +809,7 @@ public class SessionController implements Initializable {
       * call to set the pending job flag in each jobStepModel
       */
      private void setPendingJobsFlag(JobStepModel parent,JobStepModel child){
+         child.setPendingFlagProperty(Boolean.FALSE);
          if(parent.getId().equals(child.getId())){
              System.out.println("collector.Collector.mismatch():  ROOT/LEAF found: "+parent.getJobStepText());
              return;
@@ -879,7 +880,7 @@ public class SessionController implements Initializable {
      }
 
     private void setQCFlag(JobStepModel parent,JobStepModel child){
-        
+        child.setQcFlagProperty(Boolean.FALSE);
         //If Child has been traversed then return.   Create a "traversed" flag in JobStepModel and set in each time the node is returning "upwards". i.e it and all of its descendants have been traversed, set its "traversed" flag to True
         
          if(parent.getId().equals(child.getId())){
