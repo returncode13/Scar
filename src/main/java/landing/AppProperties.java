f685a6da429731e5cb0ac1b19163d1648633665c
returncode13 <returncode13@gmail.com>
2017-11-12 01:35:17 +0000

    Doubt column added to Summary Table. More work pending w.r.t classification of Doubts

--- src/main/java/landing/AppProperties.java
+++ src/main/java/landing/AppProperties.java
@@ -10,7 +10,7 @@ package landing;
  * @author sharath
  */
 public class AppProperties {
-    public static final String VERSION="1.0.5";             //www.semver.org MAJOR.MINOR.PATCH
+    public static final String VERSION="1.0.6";             //www.semver.org MAJOR.MINOR.PATCH
                                                             /*
                                                                     1. MAJOR version when you make incompatible API changes
                                                                     2. MINOR version when you add functionality in a backwards-compatible manner.
